6ef3c618b39d5d1e190fdaf58ac88d1bc01eb20d
shmuel <disraelishmuel@gmail.com>
2016-03-15 11:53:05 +0200

    intial commit al the wires are hoked up & some basic functionalty

---
+++ public/core.js
@@ -0,0 +1,45 @@
+var MEAN_app = angular.module('MEAN_app', []);
+//todo in order to undarstend this stack i will
+//add a delete btn to each todo
+//a done class state to each to do
+//add a edit mode to each todo
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
